37c58b38745dd520b18e45c993285f1fc01c26e4
Till Grenzdörffer <till.grenzdoerffer@dfki.de>
2019-01-08 16:02:08 +0100

    performance improvement + feature for recalibration of annotations

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -29,6 +29,14 @@ ros::Publisher depth_info_pub_;
 ros::Publisher rgb_img_pub_;
 ros::Publisher rgb_info_pub_;
 
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>> cloud_buffer_;
+std::vector<sensor_msgs::Image> rgb_img_buffer_;
+std::vector<sensor_msgs::CameraInfo> rgb_info_buffer_;
+std::vector<sensor_msgs::Image> depth_img_buffer_;
+std::vector<sensor_msgs::CameraInfo> depth_info_buffer_;
+
+tf::Transform offset_tf_;
+
 std::mutex config_lock;
 
 void init_tfs()
@@ -91,8 +99,82 @@ void init_tfs()
     }
 }
 
+void read_bag_msgs()
+{
+    cloud_buffer_.clear();
+    rgb_img_buffer_.clear();
+    rgb_info_buffer_.clear();
+    depth_img_buffer_.clear();
+    depth_info_buffer_.clear();
+    rosbag::Bag bag;
+    bag.open(dataset_path_ + "/scenes/" + scene_ + "/snapshots/" + std::to_string(position_) + "/" + camera_ + "/snapshot.bag");
+
+    for(rosbag::MessageInstance const m: rosbag::View(bag))
+    {
+        if (m.getTopic() == "cloud")
+        {
+            sensor_msgs::PointCloud2::ConstPtr cloud = m.instantiate<sensor_msgs::PointCloud2>();
+            if (cloud != NULL)
+            {
+                pcl::PCLPointCloud2 pcl_pc2;
+                pcl_conversions::toPCL(*cloud, pcl_pc2);
+                pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+                pcl::fromPCLPointCloud2(pcl_pc2, *temp_cloud);
+
+                pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_all_rig_frame (new pcl::PointCloud<pcl::PointXYZRGB>);
+                geometry_msgs::TransformStamped transform_msg = tf_buffer_.lookupTransform("rig_link", cloud->header.frame_id, ros::Time(0));
+                tf::StampedTransform transform_tf;
+                tf::transformStampedMsgToTF(transform_msg, transform_tf);
+                pcl_ros::transformPointCloud(*temp_cloud, *cloud_all_rig_frame, transform_tf);
+                cloud_all_rig_frame->header.frame_id = "rig_link";
+
+                cloud_buffer_.push_back(*cloud_all_rig_frame);
+            }
+        }
+        if (m.getTopic() == "rgb_img")
+        {
+            sensor_msgs::Image::ConstPtr rgb_image = m.instantiate<sensor_msgs::Image>();
+            if (rgb_image != NULL)
+            {
+                rgb_img_buffer_.push_back(*rgb_image);
+
+            }
+        }
+        if (m.getTopic() == "rgb_info")
+        {
+            sensor_msgs::CameraInfo::ConstPtr rgb_info = m.instantiate<sensor_msgs::CameraInfo>();
+            if (rgb_info != NULL)
+            {
+                rgb_info_buffer_.push_back(*rgb_info);
+
+            }
+        }
+        if (m.getTopic() == "depth_img")
+        {
+            sensor_msgs::Image::ConstPtr depth_image = m.instantiate<sensor_msgs::Image>();
+            if (depth_image != NULL)
+            {
+                depth_img_buffer_.push_back(*depth_image);
+
+            }
+        }
+        if (m.getTopic() == "depth_info")
+        {
+            sensor_msgs::CameraInfo::ConstPtr depth_info = m.instantiate<sensor_msgs::CameraInfo>();
+            if (depth_info != NULL)
+            {
+                depth_info_buffer_.push_back(*depth_info);
+
+            }
+        }
+    }
+
+    bag.close();
+}
+
 void read_ground_truth()
 {
+    ground_truth_.clear();
     if (boost::filesystem::is_regular_file(dataset_path_ + "/scenes/" + scene_ +"/recognition_truth.yaml"))
     {
         YAML::Node recog_gt = YAML::LoadFile(dataset_path_ + "/scenes/" + scene_ +"/recognition_truth.yaml");
@@ -124,9 +206,29 @@ void read_ground_truth()
 }
 
 void dynamic_callback(ycb_multicam_dataset::visualizerConfig &config) {
-    // TODO: Check scene
+
     config_lock.lock();
-    if (scene_ != config.scene || position_ != config.position)
+
+    if (camera_ != config.camera)
+    {
+        camera_ = config.camera;
+        if (position_ == config.position && scene_ == config.scene)
+        {
+            read_bag_msgs();
+        }
+    }
+
+    if (position_ != config.position)
+    {
+        position_ = config.position;
+        if (scene_ == config.scene)
+        {
+            init_tfs();
+            read_bag_msgs();
+        }
+    }
+
+    if (scene_ != config.scene)
     {
         if (boost::filesystem::is_directory(dataset_path_ + "/scenes/" + config.scene))
         {
@@ -136,75 +238,47 @@ void dynamic_callback(ycb_multicam_dataset::visualizerConfig &config) {
         {
             ROS_WARN("Configured scene does not exist.");
         }
-        position_ = config.position;
         init_tfs();
         read_ground_truth();
+        read_bag_msgs();
     }
+
+
+
+    tf::Vector3 pos;
+    pos.setX(config.pos_offset_x);
+    pos.setY(config.pos_offset_y);
+    pos.setZ(config.pos_offset_z);
+
+    tf::Quaternion q;
+    q.setX(config.rot_offset_x);
+    q.setY(config.rot_offset_y);
+    q.setZ(config.rot_offset_z);
+    q.setW(config.rot_offset_w);
+
+    offset_tf_.setOrigin(pos);
+    offset_tf_.setRotation(q);
+
     camera_ = config.camera;
     config_lock.unlock();
 }
 
-void publish_topics()
+void publish_msgs()
 {
-    rosbag::Bag bag;
-    bag.open(dataset_path_ + "/scenes/" + scene_ + "/snapshots/" + std::to_string(position_) + "/" + camera_ + "/snapshot.bag");
-
-    for(rosbag::MessageInstance const m: rosbag::View(bag))
-    {
-        if (m.getTopic() == "cloud")
+    try {
+        for (int i = 0; i < cloud_buffer_.size() ; i++)
         {
-            sensor_msgs::PointCloud2::ConstPtr cloud = m.instantiate<sensor_msgs::PointCloud2>();
-            if (cloud != NULL)
-            {
-                pcl::PCLPointCloud2 pcl_pc2;
-                pcl_conversions::toPCL(*cloud, pcl_pc2);
-                pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-                pcl::fromPCLPointCloud2(pcl_pc2, *temp_cloud);
-
-                pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_all_rig_frame (new pcl::PointCloud<pcl::PointXYZRGB>);
-                geometry_msgs::TransformStamped transform_msg = tf_buffer_.lookupTransform("rig_link", cloud->header.frame_id, ros::Time(0));
-                tf::StampedTransform transform_tf;
-                tf::transformStampedMsgToTF(transform_msg, transform_tf);
-                pcl_ros::transformPointCloud(*temp_cloud, *cloud_all_rig_frame, transform_tf);
-                cloud_all_rig_frame->header.frame_id = "rig_link";
-                cloud_pub_.publish(cloud_all_rig_frame);
-            }
-        }
-        if (m.getTopic() == "rgb_img")
-        {
-            sensor_msgs::Image::ConstPtr rgb_image = m.instantiate<sensor_msgs::Image>();
-            if (rgb_image != NULL)
-            {
-                rgb_img_pub_.publish(rgb_image);
-            }
-        }
-        if (m.getTopic() == "rgb_info")
-        {
-            sensor_msgs::CameraInfo::ConstPtr rgb_info = m.instantiate<sensor_msgs::CameraInfo>();
-            if (rgb_info != NULL)
-            {
-                rgb_info_pub_.publish(rgb_info);
-            }
-        }
-        if (m.getTopic() == "depth_img")
-        {
-            sensor_msgs::Image::ConstPtr depth_image = m.instantiate<sensor_msgs::Image>();
-            if (depth_image != NULL)
-            {
-                depth_img_pub_.publish(depth_image);
-            }
-        }
-        if (m.getTopic() == "depth_info")
-        {
-            sensor_msgs::CameraInfo::ConstPtr depth_info = m.instantiate<sensor_msgs::CameraInfo>();
-            if (depth_info != NULL)
-            {
-                depth_info_pub_.publish(depth_info);
-            }
+            cloud_pub_.publish(cloud_buffer_.at(i));
+            rgb_img_pub_.publish(rgb_img_buffer_.at(i));
+            rgb_info_pub_.publish(rgb_info_buffer_.at(i));
+            depth_img_pub_.publish(depth_img_buffer_.at(i));
+            depth_info_pub_.publish(depth_info_buffer_.at(i));
         }
     }
-
-    bag.close();
+    catch (std::out_of_range)
+    {
+        ROS_ERROR("Unequal amount of msgs in bagfile!.");
+    }
 }
 
 void publish_gt()
@@ -237,15 +311,16 @@ void publish_gt()
         geometry_msgs::TransformStamped transform_msg = tf_buffer_.lookupTransform("rig_link", "aruco_ref", ros::Time(0));
         tf::StampedTransform transform_tf;
         tf::transformStampedMsgToTF(transform_msg, transform_tf);
+        // test offsets for positions by applying additional transformation configurable by dyn reconf
+        transform_tf.setData(transform_tf*offset_tf_);
         pcl_ros::transformPointCloud(*cloud_all, *cloud_all_cam_frame, transform_tf);
         cloud_all_cam_frame->header.frame_id = "rig_link";
+        gt_pub_.publish(cloud_all_cam_frame);
 
-        publish_topics();
+        publish_msgs();
 
         config_lock.unlock();
 
-        gt_pub_.publish(cloud_all_cam_frame);
-        ros::Duration(0.5).sleep();
         ros::spinOnce();
     }
 }
